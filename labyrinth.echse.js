0c84735386bdb82692972468689644b0485429f2
Sebastian Schmittner <sebastian@schmittner.pw>
2016-12-19 11:31:59 +0100

    first reasonable level gen working

--- labyrinth.echse.js
+++ labyrinth.echse.js
@@ -47,13 +47,34 @@ var LABYRINTH =
       FLYING: 2,
 
       // labyrinth generator enum
-      RANDOM_LEVEL: 0,
+      SCATTER_LEVEL: 0,
+      DIGGY_LEVEL:1,
 
       // constants
       FIELD_SIZE: 100,
       SPEED: 150,
 
+      // a + b
+      vector_add: function(a,b)
+      {
+        if(a == undefined)
+          return b;
+        if (b == undefined)
+          return a;
 
+        var re = {x:0,y:0};
+        if(a.x != undefined)
+          re.x += a.x;
+        if(a.y != undefined)
+          re.y += a.y;
+
+        if(b.x != undefined)
+          re.x += b.x;
+        if(b.y != undefined)
+          re.y += b.y;
+
+        return re;
+      },
 
       // coin phaser game
       run_phaser: function(game, preload_function, create_function, update_function)
@@ -86,7 +107,10 @@ var LABYRINTH =
       phaser_preload: function(game){
         console.log("Phaser: Preloading graphics");
 
-        game.phaser_game.load.image("player1", "img/p1.png");
+        game.phaser_game.load.spritesheet("player1", "img/p1.png",100,100);
+
+        game.phaser_game.load.image("goal", "img/goal.png");
+
 
         game.phaser_game.load.image("mud", "img/mud.png");
         game.phaser_game.load.image("bush", "img/bush.png");
@@ -107,14 +131,31 @@ var LABYRINTH =
           LABYRINTH.coin_player(game);
 
         game.player.phaser_sprite = game.phaser_game.add.sprite(0,0,game.player.graphix);
-        game.player.phaser_sprite.width = LABYRINTH.FIELD_SIZE;
-        game.player.phaser_sprite.height = LABYRINTH.FIELD_SIZE;
 
-        game.player.phaser_sprite.x = game.player.start_x * LABYRINTH.FIELD_SIZE;
-        game.player.phaser_sprite.y = game.player.start_y * LABYRINTH.FIELD_SIZE;
+        var player_sprite = game.player.phaser_sprite;
 
-        game.phaser_game.physics.arcade.enable(game.player.phaser_sprite);
-        game.phaser_game.camera.follow(game.player.phaser_sprite);
+        // make player slightly smaller to fir through ;)
+        player_sprite.width = Math.floor(LABYRINTH.FIELD_SIZE * 0.95);
+        player_sprite.height = Math.floor(LABYRINTH.FIELD_SIZE * 0.95);
+
+        player_sprite.x = game.player.start_x * LABYRINTH.FIELD_SIZE;
+        player_sprite.y = game.player.start_y * LABYRINTH.FIELD_SIZE;
+
+        game.phaser_game.physics.arcade.enable(player_sprite);
+        game.phaser_game.camera.follow(player_sprite);
+
+        player_sprite.body.collideWorldBounds = true;
+
+        player_sprite.body.bounce.x = 0.2;
+        player_sprite.body.bounce.y = 0.2;
+
+
+        game.goal.phaser_sprite = game.phaser_game.add.sprite(0,0,game.goal.graphix);
+        game.goal.phaser_sprite.width = LABYRINTH.FIELD_SIZE * 2;
+        game.goal.phaser_sprite.height = LABYRINTH.FIELD_SIZE * 2;
+
+        game.goal.phaser_sprite.x = game.goal.x * LABYRINTH.FIELD_SIZE;
+        game.goal.phaser_sprite.y = game.goal.y * LABYRINTH.FIELD_SIZE;
 
         game.cursors = game.phaser_game.input.keyboard.createCursorKeys();
 
@@ -123,34 +164,80 @@ var LABYRINTH =
 
       phaser_update: function(game){
 
-        var body = game.player.phaser_sprite.body;
-        body.velocity.x *= 0.8;
-        body.velocity.y *= 0.8;
-
-        if (game.cursors.left.isDown)
-        {
-          body.velocity.x += -1 * LABYRINTH.SPEED;
-        }
-        else if (game.cursors.right.isDown)
-        {
-          body.velocity.x += LABYRINTH.SPEED;
-        }
-
-        if (game.cursors.up.isDown)
-        {
-          body.velocity.y += -1 * LABYRINTH.SPEED;
-        }
-        else if (game.cursors.down.isDown)
-        {
-          body.velocity.y += LABYRINTH.SPEED;
-        }
+        var next_field = {};
+        next_field.x = Math.round(game.player.phaser_sprite.x / LABYRINTH.FIELD_SIZE) * LABYRINTH.FIELD_SIZE;
+        next_field.y = Math.round(game.player.phaser_sprite.y / LABYRINTH.FIELD_SIZE) * LABYRINTH.FIELD_SIZE;
 
 
         var hit = game.phaser_game.physics.arcade.collide(game.player.phaser_sprite, game.blocking_group);
 
+
+        var player_sprite = game.player.phaser_sprite;
+        var body = player_sprite.body;
+
+        // 'friction' ;)
+        body.velocity.x = body.velocity.x * 0.8;
+        body.velocity.y = body.velocity.y * 0.8;
+
+        var active_pointer = game.phaser_game.input.activePointer;
+
+        // left/right
+        if
+          (
+            (
+              game.cursors.left.isDown
+            || (active_pointer.isDown && active_pointer.x < player_sprite.x + player_sprite.width / 2)
+            ) && !body.blocked.left
+          )
+        {
+          body.velocity.x += -1 * LABYRINTH.SPEED;
+          player_sprite.frame = 1;
+        }
+        else if
+          (
+            (
+              game.cursors.right.isDown
+            || (active_pointer.isDown && active_pointer.x > player_sprite.x + player_sprite.width / 2)
+            ) && !body.blocked.right
+          )
+        {
+          body.velocity.x += LABYRINTH.SPEED;
+          player_sprite.frame = 0;
+        }
+        else
+        {
+          // drift towards grid
+          body.velocity.x += (next_field.x + (LABYRINTH.FIELD_SIZE - game.player.phaser_sprite.width) / 2 - game.player.phaser_sprite.x) * 0.2;
+        }
+
+
+        // up/down
+        if
+          (
+            game.cursors.up.isDown
+          || (active_pointer.isDown && active_pointer.y < player_sprite.y +player_sprite.height/2)
+        )
+        {
+          body.velocity.y += -1 * LABYRINTH.SPEED;
+        }
+        else if
+          (
+            game.cursors.down.isDown
+          || (active_pointer.isDown && active_pointer.y > player_sprite.y +player_sprite.height/2)
+          )
+        {
+          body.velocity.y += LABYRINTH.SPEED;
+        }
+        else
+        {
+          body.velocity.y += (next_field.y +  (LABYRINTH.FIELD_SIZE - game.player.phaser_sprite.height) / 2 - game.player.phaser_sprite.y) * 0.2;
+        }
+
+        body.velocity.x = Math.floor(body.velocity.x);
+        body.velocity.y = Math.floor(body.velocity.y);
       },
 
-      // coin game board and generate labyrinth
+
       coin_board: function(game, width, height)
       {
         console.log("Generating board");
@@ -163,6 +250,31 @@ var LABYRINTH =
 
         game.board.width = width;
         game.board.height = height;
+        game.board.to_index = function(options)
+        {
+          if(options == undefined) return 0;
+          if(options.x == undefined) options.x = 0;
+          if(options.y == undefined) options.y = 0;
+
+          if(options.x > game.board.width || options.y > game.board.height)
+            throw "Out of board bounds!";
+
+          return options.y * game.board.width + options.x;
+        }
+
+        game.board.within_board = function(options)
+        {
+          if(options == undefined) return false;
+          if(options.x == undefined) options.x = 0;
+          if(options.y == undefined) options.y = 0;
+
+          if(options.x > game.board.width || options.y > game.board.height
+                                          || options.x < 0 || options.y < 0)
+            return false;
+
+          return true;
+        }
+
         game.board.field = [];
 
         console.log(game.board);
@@ -180,6 +292,10 @@ var LABYRINTH =
         game.player.graphix = "player1";
 
         console.log(game.player);
+
+        if(game.goal == undefined) game.goal = {};
+        game.goal.graphix = "goal";
+
         return game;
       },
 
@@ -189,40 +305,177 @@ var LABYRINTH =
       {
         console.log("Generating Labyrinth");
 
+        if(game.board == undefined) LABYRINTH.coin_board(game);
         var board = game.board;
-        if(board == undefined) board = {};
-        if(board.width == undefined) board.width = 64;
-        if(board.height == undefined) board.height = 64;
-        if(board.field == undefined) board.field = [];
 
-        if (generator_mode == undefined) generator_mode = LABYRINTH.RANDOM_LEVEL;
+        if (generator_mode == undefined) generator_mode = LABYRINTH.SCATTER_LEVEL;
 
-        var player_placed =false;
-
-        if(generator_mode == LABYRINTH.RANDOM_LEVEL)
+        if(generator_mode == LABYRINTH.SCATTER_LEVEL)
         {
-          for(var i = 0; i < board.width * board.height; i++){
-            board.field[i] = {};
-            if(Math.random() > 0.8)
+          for(var x = 0; x < game.board.width; x++)
+          {
+            for(var y = 0; y < game.board.height; y++)
             {
-              board.field[i].blocking = LABYRINTH.GROUND_BLOCKING;
-              board.field[i].graphix = "stone";
-            }
-            else
-            {
-              board.field[i].blocking = LABYRINTH.NON_BLOCKING;
-              board.field[i].graphix = "mud";
-              if(!player_placed && Math.random() > 0.9)
+              var i = game.board.to_index({x:x,y:y});
+              board.field[i] = {};
+              if
+                (
+                  x==0 || y==0 || x==game.board.width-1 || y == game.board.height-1
+                                                        || Math.random() > 0.8
+                )
               {
-                game.player.start_y = Math.floor(i/board.width);
-                game.player.start_x = i - game.player.start_y * board.width;
-                player_placed = true;
+                board.field[i].blocking = LABYRINTH.GROUND_BLOCKING;
+                board.field[i].graphix = "bush";
+              }
+              else
+              {
+                board.field[i].blocking = LABYRINTH.NON_BLOCKING;
+                board.field[i].graphix = "mud";
               }
             }
           }
+
+          game.player.start_x = Math.floor(Math.random() * (board.width-2)) + 1;
+          game.player.start_y = Math.floor(Math.random() * (board.height-2)) + 1;
+          LABYRINTH.board_rectangle(game,game.player.start_x,game.player.start_y,1,1,LABYRINTH.NON_BLOCKING,"mud");
+
+          game.goal.x = Math.floor(Math.random() * (board.width-3))+1;
+          game.goal.y =  Math.floor(Math.random() * (board.height-3))+1;
+
+          console.log("goal at", game.goal.x, game.goal.y);
+
         }
+        else if(generator_mode == LABYRINTH.DIGGY_LEVEL)
+        {
+          var PATH = 1;
+          var EARTH = 2;
+          var NEAR_PATH = 3;
+          var WALL = 4;
+
+          // start with sandbox
+          for(var x = 0; x < game.board.width; x++)
+          {
+            for(var y = 0; y < game.board.height; y++)
+            {
+              var i = game.board.to_index({x:x,y:y});
+              board.field[i] = {};
+              board.field[i].generating = EARTH;
+            }
+          }
+
+          // outer walls
+          for(var x = 0; x < game.board.width; x++)
+          {
+            board.field[game.board.to_index({x:x,y:0})].generating = WALL;
+            board.field[game.board.to_index({x:x,y:board.height-1})].generating = WALL;
+          }
+          for(var y = 0; y < game.board.height; y++)
+          {
+            board.field[game.board.to_index({x:0,y:y})].generating = WALL;
+            board.field[game.board.to_index({x:board.width -1,y:y})].generating = WALL;
+          }
+
+
+          // player starts on the left
+          game.player.start_x = 0;
+          game.player.start_y = Math.floor(Math.random() * (board.height-2)) + 1;
+          var current_field ={
+            x: game.player.start_x,
+            y: game.player.start_y
+          };
+
+          var to_index = game.board.to_index;
+          var within_board = game.board.within_board;
+
+          var generating=true;
+          while(generating)
+          {
+            var i = to_index({x:current_field.x,y:current_field.y});
+            
+            // grow path
+            board.field[i].generating = PATH;
+
+            // mark neighbours
+            var next_field = [];
+            var top = LABYRINTH.vector_add(current_field, {x:0,y:-1});
+            var bottom = LABYRINTH.vector_add(current_field, {x:0,y:1});
+            var left = LABYRINTH.vector_add(current_field, {x:-1,y:0});
+            var right = LABYRINTH.vector_add(current_field, {x:1,y:0});
+
+            var neighbours = [top,bottom,left,right];
+
+            for (var neighbour of neighbours){
+              if(within_board(neighbour)){
+                if(board.field[to_index(neighbour)].generating == EARTH)
+                {
+                  next_field.push(neighbour);
+                  board.field[to_index(neighbour)].generating = NEAR_PATH;
+                }
+                else if(board.field[to_index(neighbour)].generating == NEAR_PATH)
+                  board.field[to_index(neighbour)].generating = WALL;
+              }
+            }
+
+            // determine next step (random walk)
+            if(next_field.length >0)
+            {
+              current_field = next_field[Math.floor(Math.random() * next_field.length)];
+            }
+            else
+            {
+              // dead end -> create crossing at random possition
+              while(board.field[to_index(current_field)].generating != NEAR_PATH)
+              {
+                current_field.x = Math.floor(Math.random() * (board.width - 2)) + 1;
+                current_field.y = Math.floor(Math.random() * (board.height - 2)) + 1;
+              }
+            }
+
+            if(current_field.x == board.width - 2)
+            {
+              // reached goal zone
+              generating = false;
+
+              game.goal.x = current_field.x;
+              game.goal.y =  current_field.y;
+            }
+
+          }//wend digging path
+
+          for(var x = 0; x < game.board.width; x++)
+          {
+            for(var y = 0; y < game.board.height; y++)
+            {
+              var i = game.board.to_index({x:x,y:y});
+              if(board.field[i].generating == PATH)
+              {
+                board.field[i].blocking = LABYRINTH.NON_BLOCKING;
+                board.field[i].graphix = "mud";
+              }
+              else
+              {
+                board.field[i].blocking = LABYRINTH.GROUND_BLOCKING;
+                board.field[i].graphix = "bush";
+              }
+            }
+          }
+        }//switch generator mode
+          
+        LABYRINTH.board_rectangle(game,game.goal.x,game.goal.y,2,2,LABYRINTH.NON_BLOCKING,"mud");
 
         console.log(board);
+        return game;
+      },
+
+      board_rectangle: function(game, from_x, from_y, width, height, blocking, graphix)
+      {
+        for(var x = from_x; x < from_x + width; x++){
+          for(var y = from_y; y < from_y + height; y++){
+
+            game.board.field[x + y * game.board.width].blocking = blocking;
+            game.board.field[x + y * game.board.width].graphix = graphix;
+          }
+        }
         return game;
       },
 
@@ -253,7 +506,7 @@ var LABYRINTH =
         if(game.player == undefined)
           LABYRINTH.coin_player(game);
 
-        LABYRINTH.generate_labyrinth(game);
+        LABYRINTH.generate_labyrinth(game, LABYRINTH.DIGGY_LEVEL);
 
         for(var x = 0; x < game.board.width; x++)
         {
@@ -262,9 +515,9 @@ var LABYRINTH =
             var field = game.board.field[ y*game.board.width + x];
             var x_coord = x * LABYRINTH.FIELD_SIZE;
             var y_coord = y * LABYRINTH.FIELD_SIZE;
-            
+
             if(field.blocking < game.player.movement)
-            { 
+            {
               field.phaser_sprite = game.passable_group.create(x_coord, y_coord, field.graphix);
             }
             else
